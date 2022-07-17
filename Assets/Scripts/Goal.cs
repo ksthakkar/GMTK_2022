fa422fa8d202b386846904616d70c9198e9017e3
RJ-06 <54755468+RJ-06@users.noreply.github.com>
2022-07-17 01:45:36 -0700

    flag
    pushed flag

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider col)
+    {
+        if (col.tag == "Player") {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }   
+    }
+
+}
